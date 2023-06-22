7f26a7f63ad01385f0c136ac133d737fbde075de
Elias Tabaka <mmtabaka1@sheffield.ac.uk>
2023-06-22 13:59:16 +0100

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Elias Tabaka on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
