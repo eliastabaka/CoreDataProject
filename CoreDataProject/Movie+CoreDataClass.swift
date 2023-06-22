ffb68c65214d26d1136683197463bbe4b38fdb55
Elias Tabaka <mmtabaka1@sheffield.ac.uk>
2023-06-22 14:35:53 +0100

    Day 57: Creating NSManagedObject subclasses

---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Elias Tabaka on 22/06/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
